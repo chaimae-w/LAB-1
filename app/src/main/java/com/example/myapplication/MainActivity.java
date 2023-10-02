6229de99d879b5204affba285c34f1c50cb5609b
chaimaerachidi <chaimaerachidi78@gmail.com>
2023-10-02 22:23:44 +0300

    change onclick handle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvMain:
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tvMain = findViewById(R.id.tv,tvMain)
+        this.tvMain = findViewById(R.id.tvMain)
     }
-
-    public void bottonCLICK(View view) {
-        This.tvMain.setText("CLICK");
-    }
+    Button buttonCLICK = (Button) findViewById(R.id.buttonCLICK);
+buttonCLICK .setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            This.tvMain.setText("CLICK");
+        }
+    });
 }
+ }
