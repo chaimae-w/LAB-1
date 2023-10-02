3af7a3a2266b0a89eaa1188f60f98e68c8cd0b11
chaimaerachidi <chaimaerachidi78@gmail.com>
2023-10-02 23:23:46 +0300

    add second button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,23 @@
-package com.example.myapplication;
-
+package com.example.labonerevertrun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView tvMain;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tvMain = findViewById(R.id.tvMain)
+        this.tvMain = findViewById(R.id.tvMain);
     }
-    Button buttonCLICK = (Button) findViewById(R.id.buttonCLICK);
-buttonCLICK .setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            This.tvMain.setText("CLICK");
-        }
-    });
+
+    public void onBtnClick(View view) {
+        this.tvMain.setText("Text was changed!!!");
+    }
 }
- }
