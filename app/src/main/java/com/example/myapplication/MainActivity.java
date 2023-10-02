26e53256400c7050c219dfa13ed38140656dab86
chaimaerachidi <chaimaerachidi78@gmail.com>
2023-10-02 21:46:15 +0300

    added CLICK functuality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,14 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvMain:
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tv,tvMain)
     }
 
     public void bottonCLICK(View view) {
-        Toast. makeText ( context this, text. "Some message", Toast. LENGTH_LONG) . show () ;
+        This.tvMain.setText("CLICK");
     }
 }
